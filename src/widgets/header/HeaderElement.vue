21e4105b97ed422a4a0258e2ba325999629396e4
MassimoAurelio <massimoaureliowork@gmail.com>
2023-12-23 12:28:55 +0300

    update №1

--- src/widgets/header/HeaderElement.vue
+++ src/widgets/header/HeaderElement.vue
@@ -32,6 +32,7 @@ function toggleRegistrationPopup() {
           <div class="header_login-btn">
             <Button @click="toggleLoginPopup" size="medium" color="primary" decoration="default"
               ><Typography size="m" bold tag="span">LogIn</Typography></Button
+              
             >
             <div class="login-popup">
               <LoginPopup v-if="isLogin" :toggleRegistrationPopup="toggleRegistrationPopup" />
